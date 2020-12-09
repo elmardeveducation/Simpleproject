c8c86f79269a84bc784a2958bc16afb5c6717485
elmardeveducation <elmarh@deveducation.com>
2020-12-09 13:59:22 +0400

    change button text

--- App.js
+++ App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import {h, w} from './components/moikonstanti'
-import {Somefunc1} from './components/'
+import React, {Component} from 'react';
+import {Button, TouchableOpacity, Alert, View} from 'react-native';
+import Myfunc from './components/somefunc';
 
 
-
-export default class FlexDirectionBasics extends Component {
- 
-  componentDidMount() {
-   
-    console.log("width="+h+"hight="+w)
-  }
+export default class App extends Component {
+constructor(props){
+ super(props)  
   
+ this.state={
+  mytitle: "Hello",
+  myname : "Elmar",
+  mygender : "male"
+ }
+}
+  //data = { message: "asdaewaq" };
+//mytitle="Hello"
+//myname="Elmar"
+//mygender="male"
 
-  render() {
-    
-    
-    return (
-      // Try setting `flexDirection` to `column`.
-      
-     <Somefunc1/>   
-        
-    )
+
+  // componentDidMount () {
+  //   console.log('........my component did mount')
+  //   this.state={
+  //     mytitle: "Bye",
+  //     }
+  // }
+
+  Myclick =()=>{
+    console.log("entered")
+    this.setState({myname: 'Test'})
   }
-};
+
+
+	render(){
+   /* this.setState={
+      mytitle : "Shahriyar"   
+    }*/
+    // const { mytitle, myname, mygender } = this;
+    // const data = { mytitle, myname, mygender};
+
+		return (
+      <View style={{flex:1}}>
+        <Button title="Test" onPress={this.Myclick}/>
+      <Myfunc data={{...this.state}}/>
+
+     
+    
+      </View>
+
+
+      )
+  } 
+}
